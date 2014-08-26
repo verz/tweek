8b7f7527f2baaf793df245c8a027f051bfb9abc1
Andrii Sh <creork@gmail.com>
2014-08-26 10:43:33 +0300

    initial commit

--- NewTWTViewController.m
+++ NewTWTViewController.m
@@ -31,38 +31,38 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-	[self addInputAccessoryViewForTextView:self.textField];
+    [self addInputAccessoryViewForTextView:self.textField];
 }
 
 - (void)textViewDidBeginEditing:(UITextView *)textView;
 {
-	self.textField.text = @"";
+    self.textField.text = @"";
 }
 
 - (void)addInputAccessoryViewForTextView:(UITextView *)textView{
-	
-	UIToolbar* toolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 50)];
-	[toolbar sizeToFit];
-	toolbar.barStyle = UIBarButtonSystemItemAdd;
-	
-	
-	toolbar.items = [NSArray arrayWithObjects:[[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
-					 [[UIBarButtonItem alloc]initWithTitle:@"Post!" style:UIBarButtonItemStyleDone target:self action:@selector(returnTextView:)],
-					 nil];
-	
-	[textView setInputAccessoryView:toolbar];
-	
+    
+    UIToolbar* toolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 50)];
+    [toolbar sizeToFit];
+    toolbar.barStyle = UIBarButtonSystemItemAdd;
+    
+    
+    toolbar.items = [NSArray arrayWithObjects:[[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
+                     [[UIBarButtonItem alloc]initWithTitle:@"Post!" style:UIBarButtonItemStyleDone target:self action:@selector(returnTextView:)],
+                     nil];
+    
+    [textView setInputAccessoryView:toolbar];
+    
 }
 
 - (void) returnTextView:(UIButton *)sender{
-	
-	[self.textField resignFirstResponder];
-	
+    
+    [self.textField resignFirstResponder];
+    
 }
 
 - (void)textViewDidEndEditing:(UITextView *)textView;
 {
-	[self postTwtWithText:self.textField.text];
+    [self postTwtWithText:self.textField.text];
 }
 
 - (void)didReceiveMemoryWarning
@@ -72,37 +72,37 @@
 }
 
 -(void)postTwtWithText:(NSString*) newTwt{
-	ACAccountStore *account = [[ACAccountStore alloc] init];
-	ACAccountType *accountType = [account accountTypeWithAccountTypeIdentifier:
-								  ACAccountTypeIdentifierTwitter];
-	[account requestAccessToAccountsWithType:accountType
-									 options:nil
-								  completion:^(BOOL granted, NSError *error) {
-		if (!granted) {
-			  return;
-		}
-		
-		NSArray *accounts = [account accountsWithAccountType:accountType];
-		if (!accounts.count) {
-			return;
-		}
-		
-		ACAccount *twitterAccount = [accounts lastObject];
-		[self postUpdate:twitterAccount withText:newTwt];
-	}];
+    ACAccountStore *account = [[ACAccountStore alloc] init];
+    ACAccountType *accountType = [account accountTypeWithAccountTypeIdentifier:
+                                  ACAccountTypeIdentifierTwitter];
+    [account requestAccessToAccountsWithType:accountType
+                                     options:nil
+                                  completion:^(BOOL granted, NSError *error) {
+        if (!granted) {
+              return;
+        }
+        
+        NSArray *accounts = [account accountsWithAccountType:accountType];
+        if (!accounts.count) {
+            return;
+        }
+        
+        ACAccount *twitterAccount = [accounts lastObject];
+        [self postUpdate:twitterAccount withText:newTwt];
+    }];
 }
 
 -(void)postUpdate:(ACAccount *)account withText:(NSString*)newTwt {
-	NSDictionary* params = @{@"status": newTwt};
-	NSURL *requestURL = [NSURL URLWithString:UPDATE_URL];
-	SLRequest *postRequest = [SLRequest requestForServiceType:SLServiceTypeTwitter
-												requestMethod:SLRequestMethodPOST
-														  URL:requestURL
-												   parameters:params];
-	postRequest.account = account;
-	[postRequest performRequestWithHandler:^(NSData *responseData,
-											 NSHTTPURLResponse *urlResponse, NSError *error) {
-	//NSLog(@"Twitter HTTP response: %i", [urlResponse statusCode]);
-	}];
+    NSDictionary* params = @{@"status": newTwt};
+    NSURL *requestURL = [NSURL URLWithString:UPDATE_URL];
+    SLRequest *postRequest = [SLRequest requestForServiceType:SLServiceTypeTwitter
+                                                requestMethod:SLRequestMethodPOST
+                                                          URL:requestURL
+                                                   parameters:params];
+    postRequest.account = account;
+    [postRequest performRequestWithHandler:^(NSData *responseData,
+                                             NSHTTPURLResponse *urlResponse, NSError *error) {
+    //NSLog(@"Twitter HTTP response: %i", [urlResponse statusCode]);
+    }];
 }
 @end
--- Tweek/ViewController.m
+++ Tweek/ViewController.m
@@ -27,7 +27,7 @@
 }
 
 -(void)viewWillAppear:(BOOL)animated{
-	[self getTweetTimeLine];
+    [self getTweetTimeLine];
 }
 
 - (void)didReceiveMemoryWarning
@@ -37,79 +37,79 @@
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-	return _tweetArr.count;
+    return _tweetArr.count;
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-	static NSString* cellId = @"cellID";
-	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
-	if (cell == nil) {
-		cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
-									  reuseIdentifier:cellId];
-		cell.textLabel.numberOfLines = 0;
-		cell.textLabel.font = [UIFont fontWithName:@"Helvetica" size:10.0];
-		
-	}
-	//NSLog(@"%@",self.tweetArr.description);
+    static NSString* cellId = @"cellID";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
+                                      reuseIdentifier:cellId];
+        cell.textLabel.numberOfLines = 0;
+        cell.textLabel.font = [UIFont fontWithName:@"Helvetica" size:10.0];
+        
+    }
+    //NSLog(@"%@",self.tweetArr.description);
 
-	NSDictionary *twitDic = [_tweetArr objectAtIndex:indexPath.row];
-	NSDictionary *userDic = [twitDic objectForKey:@"user"];
+    NSDictionary *twitDic = [_tweetArr objectAtIndex:indexPath.row];
+    NSDictionary *userDic = [twitDic objectForKey:@"user"];
 
-	cell.detailTextLabel.text =[userDic objectForKey:@"name"];
-	cell.detailTextLabel.textColor = [UIColor colorWithRed:1.0 green:0 blue:0 alpha:1];
-	cell.textLabel.text = [twitDic objectForKey:@"text"];
-	
-	return cell;
+    cell.detailTextLabel.text =[userDic objectForKey:@"name"];
+    cell.detailTextLabel.textColor = [UIColor colorWithRed:1.0 green:0 blue:0 alpha:1];
+    cell.textLabel.text = [twitDic objectForKey:@"text"];
+    
+    return cell;
 }
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-	
+    
 }
 
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
 
-	return 85;
+    return 85;
 }
 
 -(void)getTweetTimeLine{
-	ACAccountStore* account = [[ACAccountStore alloc] init];
-	ACAccountType * accountType = [account accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
-	[account requestAccessToAccountsWithType:accountType
-									 options:nil
-								  completion:^(BOOL granted,NSError* error){
-		if (!granted) {
-			NSLog(@"%@",[error localizedDescription]);
-			return;
-		}
-		
-		NSArray *accounts = [account accountsWithAccountType:accountType];
-		if (!accounts.count) {
-			return;
-		}
-									  
-		[self getTimelineWithParameteres:[accounts lastObject]];
-	}];
+    ACAccountStore* account = [[ACAccountStore alloc] init];
+    ACAccountType * accountType = [account accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    [account requestAccessToAccountsWithType:accountType
+                                     options:nil
+                                  completion:^(BOOL granted,NSError* error){
+        if (!granted) {
+            NSLog(@"%@",[error localizedDescription]);
+            return;
+        }
+        
+        NSArray *accounts = [account accountsWithAccountType:accountType];
+        if (!accounts.count) {
+            return;
+        }
+                                      
+        [self getTimelineWithParameteres:[accounts lastObject]];
+    }];
 }
 
 -(void)getTimelineWithParameteres:(ACAccount*)account {
-	NSURL * requestUrl = [NSURL URLWithString:TIMELINE_URL];
-	NSDictionary* params = @{@"count": @"50", @"include_entities": @"1"};
-	SLRequest *posts = [SLRequest requestForServiceType:SLServiceTypeTwitter
-										  requestMethod:SLRequestMethodGET
-													URL:requestUrl
-											 parameters:params];
-	posts.account = account;
-	[posts performRequestWithHandler:^(NSData *response,NSHTTPURLResponse *urlResponse, NSError* errorNew){
-		self.tweetArr = [NSJSONSerialization JSONObjectWithData:response
-														options:NSJSONReadingMutableLeaves
-														  error:&errorNew];
-		
-		if (self.tweetArr.count > 0) {
-			dispatch_async(dispatch_get_main_queue(), ^{
-				[self.twtTable reloadData];
-			});
-		}
-	}];
+    NSURL * requestUrl = [NSURL URLWithString:TIMELINE_URL];
+    NSDictionary* params = @{@"count": @"50", @"include_entities": @"1"};
+    SLRequest *posts = [SLRequest requestForServiceType:SLServiceTypeTwitter
+                                          requestMethod:SLRequestMethodGET
+                                                    URL:requestUrl
+                                             parameters:params];
+    posts.account = account;
+    [posts performRequestWithHandler:^(NSData *response,NSHTTPURLResponse *urlResponse, NSError* errorNew){
+        self.tweetArr = [NSJSONSerialization JSONObjectWithData:response
+                                                        options:NSJSONReadingMutableLeaves
+                                                          error:&errorNew];
+        
+        if (self.tweetArr.count > 0) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.twtTable reloadData];
+            });
+        }
+    }];
 }
 
 @end
